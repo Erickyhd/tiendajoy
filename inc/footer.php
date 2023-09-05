d3fb85cb44b8b737319ce5a0b41ce11dc4c381d1
Erick <erick98diego@gmail.com>
2023-09-04 19:41:15 -0500

    completo proyecto

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Somos una empresa con 10 años de experencia</br>
+                    ofreciendo prendas de temporada a un precio</br>
+                    justo, con garantia .</br>
+                    </br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Av.Cajamarca N° 234 segundo piso</h4>
+                <p style="color: #FFF">Huancayo - Perú</p>
+                <p style="color: #FFF"></p>
+                <p style="font-size:20px" aria-hidden="true">  </p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  20105523 / 20105454</p> </br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp;tienda_j@joy.com.pe</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">JOY &reg; 2021</h5>
+</footer>
